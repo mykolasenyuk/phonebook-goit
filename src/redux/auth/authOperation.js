29ddd1c6a7bea13d037ea4086cf5e9059111fa69
Mykola Senyuk <73036879+mykolasenyuk@users.noreply.github.com>
2022-05-07 22:12:45 +0300

    Update authOperation.js

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+axios.defaults.baseURL = 'https://phonebook--api.herokuapp.com';
 
 const token = {
   set(token) {
